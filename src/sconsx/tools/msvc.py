4282b68743b65ff3ca967037b2545ef35aa58069
Frederic Boudon <frederic.boudon@cirad.fr>
2017-12-21 11:18:58 +0100

    add target arch for msvc

--- src/sconsx/tools/msvc.py
+++ src/sconsx/tools/msvc.py
@@ -33,6 +33,8 @@ class Msvc:
 
    def option(self, opts):
       pass
+      
+      opts.AddVariables((EnumVariable('TARGET_ARCH', 'Target Architecture','amd64' if sys.maxsize.bit_length() == 63 else 'x86', allowed_values=('x86','amd64'))))
 
    def update(self, env):
       """ Update the environment with specific flags """
